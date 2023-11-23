c21b8b1af955bf4fcbb88896292240c4c9672ce8
Tristan de Muijnck <tristandemuijnck@hotmail.com>
2023-11-23 16:53:19 +0100

    🪜 Start of upload functionality

---
+++ src/lib/organisms/UploadForm.svelte
@@ -0,0 +1,128 @@
+<script>
+	import { applyAction, enhance } from '$app/forms'
+
+	import FormField from '$lib/molecules/FormField.svelte'
+	import Button from '$lib/atoms/Button.svelte'
+
+	export let formAction
+	export let formMethod
+    export let btnText
+
+	let loading = false
+    let showUpload = true;
+    let showVerify = false;
+
+	const handleSubmit = () => {
+		loading = true
+		return async ({ result }) => {
+			await applyAction(result)
+			loading = false
+		}
+	}
+
+    const toggleUpload = () => {
+        showUpload = !showUpload;
+        showVerify = !showVerify;
+    }
+</script>
+
+<form action={formAction} method={formMethod} use:enhance={handleSubmit} class:showLogin={showUpload} on:submit={toggleUpload}>
+	<div class="form-content">
+		<FormField
+			iconSrc="/images/icons/email.svg"
+			labelFor="werkvormName"
+			labelText="Naam werkvorm"
+			inputType="text"
+			inputPlaceholder="Vul een naam in..."
+			inputName="werkvormName"
+			inputId="werkvormName"
+		/>
+        <!-- TODO Select input form field with data input from Hygraph -->
+        <FormField
+			iconSrc="/images/icons/email.svg"
+			labelFor="werkvormDesc"
+			labelText="Beschrijving werkvorm"
+			inputType="text"
+			inputPlaceholder="Vul een beschrijving in..."
+			inputName="werkvormDesc"
+			inputId="werkvormDesc"
+		/>
+        <!-- TODO Select input form field with data input from Hygraph -->
+        <FormField
+			iconSrc="/images/icons/email.svg"
+			labelFor="werkvormFaculty"
+			labelText="Beschrijving werkvorm"
+			inputType="text"
+			inputPlaceholder="Vul een faculteit in..."
+			inputName="werkvormFaculty"
+			inputId="werkvormFaculty"
+		/>
+        <!-- TODO File input form field for thumbnail, max 1 -->
+
+        <!-- TODO File input form field for video, max 1 -->
+
+        <!-- TODO File input form field for bestanden, max 3 -->
+	</div>
+
+	<Button btnType="submit" {btnText}></Button>
+</form>
+
+<!-- Check email -->
+<article class:showVerify={showVerify}>
+    <h2>Werkvorm succesvol geüpload</h2>
+</article>
+
+<style>
+	form {
+		display: none;
+		flex-direction: column;
+		row-gap: 2rem;
+	}
+
+    .showLogin {
+        display: flex;
+    }
+
+	.form-content {
+		display: flex;
+		flex-direction: column;
+		padding: 2rem;
+		background: var(--form-bg, #1e1649);
+        row-gap: 1rem;
+	}
+
+    .showVerify {
+        display: flex;
+    }
+
+    article {
+        display: none;
+        flex-direction: column;
+        justify-content: center;
+        row-gap: 0.5rem;
+        padding: 2rem 2rem;
+        background: var(--color-hva-navy);
+    }
+
+    article h2{
+        font-size: 1.2rem;
+        font-weight: 700;
+        text-align: center;
+    }
+
+    article p{
+        font-size: 0.9rem;
+        font-weight: 400;
+        text-align: center;
+    }
+
+    @media (min-width: 48rem) {
+        form {
+            width: 30rem;
+        }
+
+        article {
+            width: 30rem;
+        }
+    }
+</style>
---
+++ src/routes/upload/+page.svelte
@@ -0,0 +1,89 @@
+<script>
+	import UploadForm from '../../lib/organisms/UploadForm.svelte';
+</script>
+
+<main>
+	<section>
+		<!-- Heading -->
+		<header>
+			<h1>Werkvorm uploaden</h1>
+			<p>
+                Voeg hier jouw werkvorm toe en deel deze met collega's.
+			</p>
+		</header>
+
+		<!-- Upload Form -->
+        <UploadForm
+            formAction="/api/upload"
+            formMethod="POST"
+            btnText="Werkvorm Uploaden"
+        ></UploadForm>
+	</section>
+</main>
+
+<style>
+	main {
+		display: flex;
+		flex-direction: column;
+		padding: 0 2rem;
+	}
+
+    section {
+        display: flex;
+        flex-direction: column;
+        margin: 6rem auto 0;
+        gap: 3rem;
+        max-width: 25rem;
+    }
+
+    header {
+        display: flex;
+        flex-direction: column;
+        row-gap: 0.5rem;
+    }
+
+    h1 {
+        font-size: 1.5rem;
+        font-weight: 700;
+    }
+
+    header > p {
+        font-size: 0.9rem;
+        font-weight: 400;
+    }
+
+	@media (min-width: 48rem) {
+		main {
+			height: calc(100dvh - 9rem);
+			/* justify-content: center; */
+		}
+
+        section {
+            flex-direction: row;
+            gap: 2rem;
+            margin: 0 auto;
+            max-width: unset;
+            position: relative;
+            top: 25%;
+        }
+
+        header {
+            justify-content: center;
+            max-width: 20rem;
+        }
+	}
+
+    @media (min-width: 64rem){
+        section{
+            gap: 4rem;
+        }
+
+        header h1 {
+            font-size: 3rem;
+        }
+
+        header > p {
+            font-size: 1rem;
+        }
+    }
+</style>
