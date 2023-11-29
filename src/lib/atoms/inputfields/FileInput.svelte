860b56b37f5376f53c7e3fea1f6d6565bf21c293
Tristan de Muijnck <tristandemuijnck@hotmail.com>
2023-11-29 01:03:41 +0100

    ✨ Added V2 input fields

---
+++ src/lib/atoms/inputfields/FileInput.svelte
@@ -0,0 +1,29 @@
+<script>
+    export let inputType
+    export let inputPlaceholder
+    export let inputName
+    export let inputId
+    export let isRequired
+    export let accepted
+    export let hasMultiple
+</script>
+
+<input type="{inputType}" placeholder="{inputPlaceholder}" name="{inputName}" id="{inputId}" required={isRequired || false} accept={accepted || "image/*,video/*"} multiple={hasMultiple || false}>
+
+<style>
+    input{
+        border: solid 2px var(--border-color, #ffffff);
+        padding: 0.5rem 0.8rem;
+        background-color: var(--input-bg, #1E1649);
+        color: var(--input-color, #ffffff);
+    }
+
+    input::placeholder {
+        color: rgba(255, 255, 255, 0.4);
+        font-style: italic;
+    }
+
+    input:focus {
+        outline: var(--btn-focus, #F25379) solid 2px;
+    }
+</style>
